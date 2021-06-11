30e9e73c0a8954d645b56ac90aa3f7588e98d264
Roman-Koval <kovalrv83@gmail.com>
2021-06-11 17:49:38 +0300

    my tree commit

--- js/task-08.js
+++ js/task-08.js
@@ -1,19 +1,41 @@
-const textInput = document.querySelector(".text-input");
-const setFocusBtn = document.querySelector('[data-action="render"]');
-const removeFocusBtn = document.querySelector('[data-action="destroy"]');
+const addEl = document.querySelector('[data-action="render"]');
+const deleteEl = document.querySelector('[data-action="destroy"]');
+const boxesEl = document.querySelector('#boxes');
+const inputEl = document.querySelector('#controls').firstElementChild;
 
-setFocusBtn.addEventListener("click", () => {
-  textInput.focus();
-});
+let amount = 0;
+let size = 30;
 
-removeFocusBtn.addEventListener("click", () => {
-  textInput.blur();
-});
 
-textInput.addEventListener("focus", (event) => {
-  textInput.value = "This input has focus";
-});
+inputEl.addEventListener('input', (e) => {
+	console.log(e.currentTarget.value)
+	amount = e.currentTarget.value;
+	return amount;
+})
 
-textInput.addEventListener("blur", (event) => {
-  textInput.value = "";
-});
+addEl.addEventListener('click', (e) => {
+	console.log(e.target)
+	
+	for (let i = 0; i < amount; i += 1) {
+		
+		const divEl = document.createElement('div');
+		divEl.style.backgroundColor = `#${Math.floor(Math.random() * 8 ** 8).toString(16)}`;
+		
+		
+		divEl.style.width = `${size}px`;
+		divEl.style.height = `${size}px`;
+		size += 10;
+
+		divEl.style.margin = '20px';
+		console.log(divEl.style.height);
+		boxesEl.append(divEl);
+	}
+})
+
+deleteEl.addEventListener('click', () => {
+	for (let i = 0; i < amount; i += 1) {
+		if(boxesEl.hasChildNodes())
+		boxesEl.removeChild(boxesEl.firstChild);
+	}
+	inputEl.value = "";
+}) 
